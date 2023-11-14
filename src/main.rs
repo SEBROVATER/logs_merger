6d9696160f909db2fffcc763934b6e8553513180
SEBROVATER <sebrovskiy.k@gmail.com>
2023-11-14 20:12:02 +0300

    added "output" argument and file name generation, added writing to file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{fs, io, mem};
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::BufRead;
+use std::io::{BufRead, LineWriter, Write};
 use std::path::PathBuf;
 
 use chrono::NaiveDateTime;
@@ -8,13 +9,80 @@ use clap::Parser;
 use glob::Pattern;
 use regex::{escape, Regex};
 
+fn get_common_substring(str1: &str, str2: &str) -> String {
+    let first_sequence: Vec<char> = str1.chars().collect();
+    let second_sequence: Vec<char> = str2.chars().collect();
+
+    let mut best_i: usize = 0;
+    let mut best_j: usize = 0;
+    let mut best_size: usize = 0;
+
+    let mut second_sequence_elements = HashMap::new();
+    for (i, item) in second_sequence.iter().enumerate() {
+        let counter = second_sequence_elements
+            .entry(item)
+            .or_insert_with(Vec::new);
+        counter.push(i);
+    }
+
+    let mut j2len: HashMap<usize, usize> = HashMap::new();
+    for (i, item) in first_sequence
+        .iter()
+        .enumerate()
+    {
+        let mut new_j2len: HashMap<usize, usize> = HashMap::new();
+        if let Some(indexes) = second_sequence_elements.get(item) {
+            for j in indexes {
+                let j = *j;
+                let mut size = 0;
+                if j > 0 {
+                    if let Some(k) = j2len.get(&(j - 1)) {
+                        size = *k;
+                    }
+                }
+                size += 1;
+                new_j2len.insert(j, size);
+                if size > best_size {
+                    best_i = i + 1 - size;
+                    best_j = j + 1 - size;
+                    best_size = size;
+                }
+            }
+        }
+        j2len = new_j2len;
+    }
+
+    for _ in 0..2 {
+        while best_i > 0 && best_j > 0 &&
+            first_sequence.get(best_i - 1) == second_sequence.get(best_j - 1)
+        {
+            best_i -= 1;
+            best_j -= 1;
+            best_size += 1;
+        }
+        while best_i + best_size < first_sequence.len()
+            && best_j + best_size < second_sequence.len()
+            && first_sequence.get(best_i + best_size) == second_sequence.get(best_j + best_size)
+        {
+            best_size += 1;
+        }
+    }
+
+    let res = String::from_iter(&first_sequence[best_i..(best_i + best_size)]);
+    res
+}
+
+
 #[derive(Parser)]
 #[command(author, version, about)]
 #[command(long_about = "Merges log fields by its timestamps")]
 struct Cli {
-
     /// Directory with log files
     dir: PathBuf,
+
+    /// Name for generated file
+    #[arg(long, short, value_name = "OUTPUT")]
+    output: Option<String>,
 
     /// Regular expression to find timestamps and detect multiline logs
     ///
@@ -36,7 +104,6 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
-
     let logs_path = cli.dir;
 
     let strftime = match cli.strftime {
@@ -75,29 +142,53 @@ fn main() {
     let logs_path = fs::canonicalize(logs_path).expect("Can't absolutize path");
 
 
-    if !logs_path.join("merged").exists() {
-        fs::create_dir(logs_path.join("merged")).expect("Can't create 'merged' dir");
-    }
 
 
     let filter = Pattern::new(&cli.filter).expect("Invalid filter glob pattern");
 
-    let logs_paths = fs::read_dir(&logs_path)
+    let logs_paths: Vec<PathBuf> = fs::read_dir(&logs_path)
         .expect("Can't iterate over dir")
-        .filter(| path | {
+        .filter(|path| {
             match path {
-                Err(E) => {
+                Err(_) => {
                     panic!("Can't iterate over dir");
-                },
-                Ok(path) => { filter.matches_path(&path.path()) },
+                }
+                Ok(path) => { filter.matches_path(&path.path()) }
             }
-        });
+        }).map(|dir_entry| dir_entry.unwrap().path()).collect();
+
+
+    let file_name = match cli.output {
+        None => {
+            let first_path = match logs_paths.iter().peekable().next() {
+                None => return,
+                Some(path) => path,
+            };
+
+            logs_paths.iter().map(|path| {
+                path
+                    .file_name().unwrap()
+                    .to_str().unwrap()
+            })
+                .fold(first_path.file_name().unwrap().to_str().unwrap().to_string(),
+                      |path1, path2| get_common_substring(path1.as_str(), path2))
+                .trim_start_matches('_').to_string()
+        }
+        Some(file_name) => {
+            if file_name.starts_with("./") || file_name.starts_with("/") {
+                println!("Don't use paths for output name");
+                return;
+            };
+            file_name
+        }
+    };
+    dbg!(&file_name);
+    if !logs_path.join("merged").exists() {
+        fs::create_dir(logs_path.join("merged")).expect("Can't create 'merged' dir");
+    }
 
     let mut logs_iterators = vec![];
-
-    for dir_entry in  logs_paths {
-        let dir_entry = dir_entry.unwrap();
-        let path = dir_entry.path();
+    for path in logs_paths.iter() {
         dbg!(&path);
 
         let file = File::open(&path).unwrap();
@@ -137,6 +228,11 @@ fn main() {
         logs_iterators.push(logs);
     }
 
+    let output_path = logs_path.join("merged").join(file_name);
+
+    let file = File::create(output_path).expect("Can't create file to write");
+    let mut file = LineWriter::new(file);
+
     let mut current_logs: Vec<Vec<String>> = vec![];
     let mut current_timestamps: Vec<i64> = vec![];
 
@@ -168,7 +264,13 @@ fn main() {
 
         let max_log = current_logs.get(max_i).unwrap();
         println!("{:?}", &max_log);
-
+        for log in max_log.iter() {
+            if log.is_empty() {
+                continue
+            };
+            file.write_all(log.as_bytes()).expect("Can't write line to file");
+            file.write_all(b"\n").expect("Can't write line to file");
+        };
 
         let it = logs_iterators.get_mut(max_i).unwrap();
         match it.next() {
