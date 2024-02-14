581c65df5b63a7b8e80e28abc9e86f3e6154617f
SEBROVATER <sebrovskiy.k@gmail.com>
2024-02-15 01:52:51 +0600

    NEW: added logger

---
+++ src/logger.rs
@@ -0,0 +1,14 @@
+use std::env;
+
+
+pub fn set_logger(verbosity: u8) {
+
+    match verbosity {
+        0 => env::set_var("RUST_LOG", "error"),
+        1 => env::set_var("RUST_LOG", "warn"),
+        2 => env::set_var("RUST_LOG", "info"),
+        _ => env::set_var("RUST_LOG", "debug"),
+    }
+
+    env_logger::init();
+}
